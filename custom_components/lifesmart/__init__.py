69abd29e068a38b4d6639f5f53407afd67028c69
Maple Gao <esanisa@gmail.com>
2025-04-24 16:42:32 +0800

    ✨ [2025-04-24] 添加新特性: unload when WS stops

--- custom_components/lifesmart/__init__.py
+++ custom_components/lifesmart/__init__.py
@@ -216,11 +216,21 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(
         entry, SUPPORTED_PLATFORMS
     )
+
+    # 停止WebSocket状态管理器
+    if state_manager := hass.data[DOMAIN].get(LIFESMART_STATE_MANAGER):
+        state_manager.stop()
+
     if isinstance(
         hass.data[DOMAIN][entry.entry_id]["client"],
         lifesmart_protocol.LifeSmartLocalClient,
     ):
         await hass.data[DOMAIN][entry.entry_id]["client"].async_disconnect(None)
+
+    # 清理数据
+    if unload_ok and entry.entry_id in hass.data[DOMAIN]:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
     return unload_ok
 
 
